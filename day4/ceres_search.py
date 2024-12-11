b7f8f2656fb74cdcf18a20acb4c125311f942933
Robert Lewis <rablewis@algo-pop.com>
2024-12-11 22:29:23 +0000

    day 4 part 2

--- day4/ceres_search.py
+++ day4/ceres_search.py
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2024/day/4
+
 input = '''AXSASXXMASAMXMXSASMMXMSAMXMXXMASMXMXMSMSMXMAXMSMSMSMXXMXMXMASXMASXMXSXSASXSXSSSSSSMSSMXMAASAMXXSSSMMXMMMSXSAXMASXMMXSXMAXSAMXAMXXMASMMXXSAXS
 SXXAAMAXMAXMAAAXXMAXAXSMXAMMMMSMMMMAMAMAMSMSMXAAAAASAMMAMXMASAMXMASXSAMXMAAAMAAAAXXAAMSSMXMAXMAMAAXSASAAMMAMSAAXSASASASAMXSXMXSAMMAMAMSSMSAA
 XXMMMAMMXMMSSMSSXXAMSMMMSMSAMXASAAMAXAMAMAAAASMSMSMSXASASMSASXMASXSAMSMAMAMSMMMMMMMXSMAASMSSMAAMMMMSASMSMSSMSMSSSXMXSAMXSAMMMAMXMXAXAMXAAMMM
@@ -171,93 +173,130 @@ def load_puzzle(input):
 
 
 def find_starting_candidates(puzzle, s, length):
+    matches = find_matching_cells(puzzle, s)
+
     candidates = dict()
     distance = length - 1
+    for candidate in matches:
+        x = candidate[0]
+        y = candidate[1]
+        possible_directions = [1,1,1,1,1,1,1,1]  # could optimise with bitflags
+        if y - distance < 0:
+            possible_directions[7] = 0
+            possible_directions[0] = 0
+            possible_directions[1] = 0
+
+        if x + distance >= len(puzzle[0]):
+            possible_directions[1] = 0
+            possible_directions[2] = 0
+            possible_directions[3] = 0
+        
+        if y + distance >= len(puzzle):
+            possible_directions[3] = 0
+            possible_directions[4] = 0
+            possible_directions[5] = 0
+        
+        if x - distance < 0:
+            possible_directions[5] = 0
+            possible_directions[6] = 0
+            possible_directions[7] = 0
+
+        candidates[(x, y)] = possible_directions
+
+    return candidates
+
+def find_matching_cells(puzzle, s):
+    matches = []
     for y in range(len(puzzle)):
         for x in range(len(puzzle[0])):
             if puzzle[y][x] == s:
-                possible_directions = [1,1,1,1,1,1,1,1]  # could optimise with bitflags
-                if y - distance < 0:
-                    possible_directions[7] = 0
-                    possible_directions[0] = 0
-                    possible_directions[1] = 0
+                matches.append((x, y))
+    return matches
 
-                if x + distance >= len(puzzle[0]):
-                    possible_directions[1] = 0
-                    possible_directions[2] = 0
-                    possible_directions[3] = 0
-                
-                if y + distance >= len(puzzle):
-                    possible_directions[3] = 0
-                    possible_directions[4] = 0
-                    possible_directions[5] = 0
-                
-                if x - distance < 0:
-                    possible_directions[5] = 0
-                    possible_directions[6] = 0
-                    possible_directions[7] = 0
 
-                candidates[(x, y)] = possible_directions
+def remove_margin_cells(puzzle, cells, margin):
+    filtered_cells = []
+    for cell in cells:
+        if not is_in_margin(puzzle, cell, margin):
+            filtered_cells.append(cell)
+    
+    return filtered_cells
 
-    return candidates
+
+def is_in_margin(puzzle, cell, margin):
+    x = cell[0]
+    y = cell[1]
+    if x - margin < 0:
+        return True
+    
+    if y - margin < 0:
+        return True
+    
+    if x + margin >= len(puzzle[0]):
+        return True
+    
+    if y + margin >= len(puzzle):
+        return True
+    
+    return False
+
+
+def filter_non_x_mas_es(puzzle, centers):
+    filtered = []
+    for center_A in centers:
+        if has_nw_se_mas(puzzle, center_A) and has_ne_sw_mas(puzzle, center_A):
+            filtered.append(center_A)
+
+    return filtered
+
+
+def has_nw_se_mas(puzzle, center_A):
+    return has_nw_mas(puzzle, center_A) or has_se_mas(puzzle, center_A)
+
+
+def has_ne_sw_mas(puzzle, center_A):
+    return has_ne_mas(puzzle, center_A) or has_sw_mas(puzzle, center_A)
+
+
+def has_nw_mas(puzzle, center_A):
+    return has_mas(puzzle, center_A, 7)
+
+
+def has_se_mas(puzzle, center_A):
+    return has_mas(puzzle, center_A, 3)
+
+
+def has_ne_mas(puzzle, center_A):
+    return has_mas(puzzle, center_A, 1)
+
+
+def has_sw_mas(puzzle, center_A):
+    return has_mas(puzzle, center_A, 5)
+
+
+def has_mas(puzzle, center_A, direction):
+    if not has_neighbour(puzzle, center_A, direction, 1, 'M'):
+        return False
+    
+    opposite = (direction + 4) % 8
+    if not has_neighbour(puzzle, center_A, opposite, 1, 'S'):
+        return False
+    
+    return True
 
 
 def update_candidates(puzzle, candidates, s, distance):
     inactive_candidates = []
     for candidate in candidates.keys():
-        x = candidate[0]
-        y = candidate[1]
-
         directions = candidates[candidate]
 
         active = False
-        if directions[0] != 0:
-            if puzzle[y - distance][x] == s:
-                active = True
-            else:
-                directions[0] = 0
-
-        if directions[1] != 0:
-            if puzzle[y - distance][x + distance] == s:
-                active = True
-            else:
-                directions[1] = 0
-
-        if directions[2] != 0:
-            if puzzle[y][x + distance] == s:
-                active = True
-            else:
-                directions[2] = 0
-
-        if directions[3] !=0:
-            if puzzle[y + distance][x + distance] == s:
-                active = True
-            else:
-                directions[3] = 0
-
-        if directions[4] != 0:
-            if puzzle[y + distance][x] == s:
-                active = True
-            else:
-                directions[4] = 0
-
-        if directions[5] != 0:
-            if puzzle[y + distance][x - distance] == s:
-                active = True
-            else:
-                directions[5] = 0
-
-        if directions[6] != 0:
-            if puzzle[y][x - distance] == s:
-                active = True
-            else:
-                directions[6] = 0
-
-        if directions[7] != 0:
-            if puzzle[y - distance][x - distance] == s:
-                active = True
-            else:
-                directions[7] = 0
+        for d in range(8):
+            if directions[d] != 0:
+                if has_neighbour(puzzle, candidate, d, distance, s):
+                    active = True
+                else:
+                    directions[d] = 0
 
         if not active:
             inactive_candidates.append(candidate)
@@ -268,18 +307,60 @@ def update_candidates(puzzle, candidates, s, distance):
     return candidates
 
 
+def has_neighbour(puzzle, position, direction, distance, s):
+    x0 = position[0]
+    y0 = position[1]
+    if direction == 0:
+        x = x0
+        y = y0 - distance
+    elif direction == 1:
+        x = x0 + distance
+        y = y0 - distance
+    elif direction == 2:
+        x = x0 + distance
+        y = y0
+    elif direction == 3:
+        x = x0 + distance
+        y = y0 + distance
+    elif direction == 4:
+        x = x0
+        y = y0 + distance
+    elif direction == 5:
+        x = x0 - distance
+        y = y0 + distance
+    elif direction == 6:
+        x = x0 - distance
+        y = y0
+    elif direction == 7:
+        x = x0 - distance
+        y = y0 - distance
+    
+    return puzzle[y][x] == s
+
+
 
 puzzle = load_puzzle(input)
 
-candidates = find_starting_candidates(puzzle, 'X', 4)
-candidates = update_candidates(puzzle, candidates, 'M', 1)
-candidates = update_candidates(puzzle, candidates, 'A', 2)
-candidates = update_candidates(puzzle, candidates, 'S', 3)
+# # TASK 1
+# candidates = find_starting_candidates(puzzle, 'X', 4)
+# candidates = update_candidates(puzzle, candidates, 'M', 1)
+# candidates = update_candidates(puzzle, candidates, 'A', 2)
+# candidates = update_candidates(puzzle, candidates, 'S', 3)
 
-count = 0
-for k in candidates.keys():
-    count += reduce(lambda i, j: i + j, candidates[k], 0)
+# count = 0
+# for k in candidates.keys():
+#     count += reduce(lambda i, j: i + j, candidates[k], 0)
 
-print(count)
+# print(count)
 
 # task 1 answer is 18 for example_input
+
+# TASK 2
+
+centers = find_matching_cells(puzzle, 'A')
+centers = remove_margin_cells(puzzle, centers, 1)
+
+x_mas_centers = filter_non_x_mas_es(puzzle, centers)
+print(len(x_mas_centers))
+
+# task 2 answer is 9 for example_input
