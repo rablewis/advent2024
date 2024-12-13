cc65bb709e8360e79d63a3b2504d8053cd38c73b
Robert Lewis <rablewis@algo-pop.com>
2024-12-13 04:32:39 +0000

    day 2 - setting up

---
+++ day2/red_nosed_reports.py
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2024/day/2
+
+example_input = '''7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9'''
+
+
+
+
+
+# part 1 answer is 2 using the example input
